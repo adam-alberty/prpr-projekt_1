33b788aa629a6a02d936d4851703e06bea955135
adam-alberty <xalberty@stuba.sk>
2023-11-01 13:26:40 +0100

    Remake to not use 3d array

--- main.c
+++ main.c
@@ -10,12 +10,9 @@
 #define DATUM_MERANIA 5
 
 // Prints items from dynamic array
-void print_items(char ***items_global, int item_count_global) {
-    for (int i = 0; i < item_count_global; i++) {
-        for(int j = 0; j < 6; j++) {
-            printf("%s\n", items_global[i][j]);
-        }
-        printf("\n");
+void print_items(char **items_global, int item_count_global) {
+    for (int i = 0; i < item_count_global * 6; i++) {
+        printf("%s\n", items_global[i]);
     }
 }
 
@@ -24,13 +21,13 @@ void print_file(FILE **fp) {
     char line[20];
 
     fseek(*fp, 0, SEEK_SET);
-    while (fgets(line, 20, *fp) != NULL) {
+    while (fgets(line, sizeof(line) - 1, *fp) != NULL) {
         printf("%s", line);
     }
 }
 
 // Opens file and reads it
-void open_file(FILE **fp, char ***items_global, int item_count_global) {
+void open_file(FILE **fp, char **items_global, int item_count_global) {
     if (*fp != NULL) {
         if (items_global != NULL) {
             print_items(items_global, item_count_global);
@@ -40,12 +37,11 @@ void open_file(FILE **fp, char ***items_global, int item_count_global) {
         return;
     }
 
-    *fp = fopen("./dataloger.txt", "r");
+    *fp = fopen("dataloger.txt", "r");
     if (*fp == NULL) {
         printf("Neotvoreny subor\n");
         return;
     }
-
     print_file(fp);
 }
 
@@ -53,21 +49,18 @@ void open_file(FILE **fp, char ***items_global, int item_count_global) {
 int count_items(FILE *fp) {
     fseek(fp, 0, SEEK_SET);
 
-    int item_count = 0;
+    int line_count = 1;
     char c;
     while((c = fgetc(fp)) != EOF) {
         if (c == '\n') {
-            item_count++;
+            line_count++;
         }
     }
-    return item_count / 6;
+    return line_count / 7;
 }
 
-void deallocate_arrays(char ****items_global, int *item_count_global) {
-    for (int i = 0; i < *item_count_global; i++) {
-        for (int j = 0; j < 6; j++) {
-            free((*items_global)[i][j]);
-        }
+void deallocate_arrays(char ***items_global, int *item_count_global) {
+    for (int i = 0; i < *item_count_global * 6; i++) {
         free((*items_global)[i]);
     }
     free(*items_global);
@@ -77,7 +70,7 @@ void deallocate_arrays(char ****items_global, int *item_count_global) {
 }
 
 // Allocate arrays dynamically   
-void allocate_arrays(FILE **fp, char ****items_global, int *item_count_global) {
+void allocate_arrays(FILE **fp, char ***items_global, int *item_count_global) {
     if (*fp == NULL) {
         printf("Neotvoreny subor\n");
         return;
@@ -90,146 +83,149 @@ void allocate_arrays(FILE **fp, char ****items_global, int *item_count_global) {
     int item_count = count_items(*fp);
     fseek(*fp, 0, SEEK_SET);
 
-    // Allocate main array - items
-    char ***items = (char***)malloc(sizeof(char**) * item_count);
+    // Allocate main array - measurements (without \n)
+    char **items = (char**)malloc(sizeof(char*) * (item_count * 6));
 
     if (items == NULL) {
         printf("Nepodarilo sa alokovat");
         exit(1);
     }
 
-    for (int i = 0; i < item_count; i++) {
-        // Allocate array of strings - item
-        char **item = (char**)malloc(sizeof(char*) * 6);
-        if (item == NULL) {
+    int item_idx = 0;
+    for (int i = 0; i < item_count * 7; i++) {
+        // Newline every 6th line
+        if ((i + 1) % 7 == 0) {
+            char throwaway[30];
+            fgets(throwaway, sizeof(throwaway), *fp);
+            continue;
+        }
+
+        char buff[30];
+        fgets(buff, sizeof(buff), *fp);
+
+        // Allocate memory for measurement attribute
+        items[item_idx] = malloc(sizeof(char) * strlen(buff) + 1);
+        if (items[item_idx] == NULL) {
             printf("Nepodarilo sa alokovat");
             exit(1);
         }
-
-        for (int j = 0; j < 6; j++) {
-            // Allocate memory for string - line;
-            char *string = malloc(sizeof(char) * 20);
-            if (string == NULL) {
-                printf("Nepodarilo sa alokovat");
-                exit(1);
-            }
-
-            fgets(string, 20, *fp);
-            string[strlen(string) - 1] = '\0';
-            item[j] = string;
-        }
-        char throwaway[10];
-        fgets(throwaway, 10, *fp);
-        items[i] = item;
+        strcpy(items[ item_idx], buff);
+        items[item_idx][strlen(items[ item_idx]) - 1] = '\0';
+        printf("%s\n", items[ item_idx]);
+        item_idx++;
     }
 
     *items_global = items;
     *item_count_global = item_count;
 }
 
-void ciachovanie(char ***items_global, int item_count_global) {
-    if (items_global == NULL) {
-        printf("Polia nealokovane\n");
-        return;
-    }
+// void ciachovanie(char ***items_global, int item_count_global) {
+//     if (items_global == NULL) {
+//         printf("Polia nealokovane\n");
+//         return;
+//     }
     
-    int Y;
+//     int Y;
 
-    printf("Zadaj pocet mesiacov: ");
-    scanf("%d", &Y);
-    FILE *fp = fopen("./ciachovanie.txt", "r");
-    if (fp == NULL) {
-        return;
-    };
+//     printf("Zadaj pocet mesiacov: ");
+//     scanf("%d", &Y);
+//     FILE *fp = fopen("./ciachovanie.txt", "r");
+//     if (fp == NULL) {
+//         return;
+//     };
 
 
-    for (int i = 0; i < item_count_global; i++) {
-        fseek(fp, 0, SEEK_SET);
-        char line[20];
+//     for (int i = 0; i < item_count_global; i++) {
+//         fseek(fp, 0, SEEK_SET);
+//         char line[20];
 
-        int k = -1;
-        while (fgets(line, 20, fp) != NULL) {
-            k = (k + 1) % 3;
+//         int k = -1;
+//         while (fgets(line, 20, fp) != NULL) {
+//             k = (k + 1) % 3;
 
-            if (k == 2) {
-                fgets(line, 20, fp);
-                continue;
-            }
+//             if (k == 2) {
+//                 fgets(line, 20, fp);
+//                 continue;
+//             }
 
-            if (k == 0) {
-                printf("Name: %s\n", items_global[i][ID_MODULU]);
-                printf("LINE ID MODULU: %s", line);
-            }
+//             if (k == 0) {
+//                 printf("Name: %s\n", items_global[i][ID_MODULU]);
+//                 printf("LINE ID MODULU: %s", line);
+//             }
 
-            if (k == 1) {
-                printf("ARRAY DATUM: %s\n", items_global[i][DATUM_MERANIA]);
-                printf("LINE DATUM: %s", line);
-            }
-        }
-    }
-}
+//             if (k == 1) {
+//                 printf("ARRAY DATUM: %s\n", items_global[i][DATUM_MERANIA]);
+//                 printf("LINE DATUM: %s", line);
+//             }
+//         }
+//     }
+// }
 
-int count_items_with_id(char *module_id, char *unit, char ***items_global, int item_count_global) {
-    int count = 0;
-    for (int i = 0; i < item_count_global; i++) {
-        if (strcmp(items_global[i][ID_MODULU], module_id) == 0 && strcmp(items_global[i][TYP_VELICINY], unit) == 0)
-            count++;
-    }
+// int count_items_with_id(char *module_id, char *unit, char ***items_global, int item_count_global) {
+//     int count = 0;
+//     for (int i = 0; i < item_count_global; i++) {
+//         if (strcmp(items_global[i][ID_MODULU], module_id) == 0 && strcmp(items_global[i][TYP_VELICINY], unit) == 0)
+//             count++;
+//     }
 
-    return count;
-}
+//     return count;
+// }
 
-void vystup(char ***items_global, int item_count_global) {
-    if (items_global == NULL) {
-        printf("Polia nie su vytvorene\n");
-        return;
-    }
+// void vystup(char ***items_global, int item_count_global) {
+//     if (items_global == NULL) {
+//         printf("Polia nie su vytvorene\n");
+//         return;
+//     }
 
-    char module_id[6];
-    char unit[3];
+//     char module_id[6];
+//     char unit[3];
 
-    scanf("%s %s", module_id, unit);
-    int count = count_items_with_id(module_id, unit, items_global, item_count_global);
-    if (count == 0) {
-        printf("Pre dany vstup neexistuju zaznamy");
-    }
+//     scanf("%s %s", module_id, unit);
+//     int count = count_items_with_id(module_id, unit, items_global, item_count_global);
+//     if (count == 0) {
+//         printf("Pre dany vstup neexistuju zaznamy");
+//     }
 
-    char **arr = malloc(sizeof(char *) * count);
+//     char **arr = malloc(sizeof(char *) * count);
 
-    int arr_i = 0; 
-    for (int i = 0; i < item_count_global; i++) {
-        if (strcmp(items_global[i][ID_MODULU], module_id) == 0 && strcmp(items_global[i][TYP_VELICINY], unit) == 0) {
-            char *value = malloc(sizeof(char) * 50);
-            strcpy(items_global[], value);
-            arr[arr_i] = value; 
-            arr_i++;
-        }
-    }
-}
+//     int arr_i = 0; 
+//     for (int i = 0; i < item_count_global; i++) {
+//         if (strcmp(items_global[i][ID_MODULU], module_id) == 0 && strcmp(items_global[i][TYP_VELICINY], unit) == 0) {
+//             char *value = malloc(sizeof(char) * 50);
+//             // strcpy(items_global[], value);
+//             arr[arr_i] = value; 
+//             arr_i++;
+//         }
+//     }
+// }
 
 int main(void) {
     char command;
     FILE *fp = NULL;
-    char ***items = NULL;
+    char **items = NULL;
     int item_count = 0;
 
     // Hlavny cyklus na zadavanie prikazov
     while (command = getchar()) {
         switch(command) {
             case 'v':
+                printf("----------------OPEN FILE------------\n");
                 open_file(&fp, items, item_count);
+                printf("----------------OPEN FILE------------\n");
                 break;
             case 'n':
+                printf("----------------ALLOCATE ARRAYS------------\n");
                 allocate_arrays(&fp, &items, &item_count);
+                printf("----------------ALLOCATE ARRAYS------------\n");
                 break;
-            case 'c':
-                // TODO
-                ciachovanie(items, item_count);
-                break;
-            case 's':
-                // TODO
-                vystup(items, item_count);
-                break;
+            // case 'c':
+            //     // TODO
+            //     ciachovanie(items, item_count);
+            //     break;
+            // case 's':
+            //     // TODO
+            //     vystup(items, item_count);
+            //     break;
         }
     }    
     return 0;
